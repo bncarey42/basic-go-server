1b82b066dcb9b876e2fd53d0a3ce283290233374
ben <bncarey42@users.noreply.github.com>
2020-10-04 17:46:32 -0500

    add params function

--- main.go
+++ main.go
@@ -37,6 +37,35 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"message": "ERROR NOT FOUND"}`))
 }
 
+func params(w http.ResponseWriter, r *http.Request) {
+    pathParams := mux.Vars(r)
+    w.Header().Set("Content-Type", "application/json")
+    userID := -1
+    var err error
+    if val, ok := pathParams["userID"]; ok {
+        userID, err = strconv.Atoi(val)
+        if err != nil {
+            w.WriteHeader(http.StatusInternalServerError)
+            w.Write([]byte(`{"message": "need a number"}`))
+            return
+        }
+    }
+    commentID := -1
+    if val, ok := pathParams["commentID"]; ok {
+        commentID, err = strconv.Atoi(val)
+        if err != nil {
+            w.WriteHeader(http.StatusInternalServerError)
+            w.Write([]byte(`{"message": "need a number"}`))
+            return
+        }
+    }
+    query := r.URL.Query()
+    location := query.Get("location")
+    w.Write([]byte(fmt.Sprintf(`{"userID": %d, "commentID": %d, "location": "%s" }`, userID, commentID, location)))
+}
+
+
+// https://dev.to/moficodes/build-your-first-rest-api-with-go-2gcj
 func main() {
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api/v1").Subrouter()
@@ -45,5 +74,6 @@ func main() {
 	api.HandleFunc("", put).Methods(http.MethodPut)
 	api.HandleFunc("", delete).Methods(http.MethodDelete)
 	api.HandleFunc("", notFound)
+	api.HandleFunc("/user/{userID}/comment/{commentID}", params).Methods(http.MethodGet)
 	log.Fatal(http.ListenAndServe(":9090", r))
 }
